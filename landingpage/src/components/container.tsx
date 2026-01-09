ddb266723c936669e8cb9ddc114145f88570009e
Pradeep yadav <pradeep20020102@gmail.com>
2026-01-09 21:02:49 +0545

    cotainer is made for hero section along with side lines

--- landingpage/src/components/container.tsx
+++ landingpage/src/components/container.tsx
@@ -1,8 +1,11 @@
+import { cn } from "@/lib/utilis"
+import React from "react"
 
-export const Container = ({children} :{children:React.ReactNode}) => {
-    return (
-        <div className="container">
-            {children}
-        </div>
-    )
+export const Container = ({children, className}:{
+    children:React.ReactNode,
+    className?:string
+}) => {
+return <div className={cn("max-w-5xl  mx-auto w-full h-screen border-l border-r border-x-neutral-300/60 via-neutral-200 to-transparent ", className)}>
+    {children}
+</div>
 }
