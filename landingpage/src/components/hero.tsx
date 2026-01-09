4ba7cfc9fe4471974a159bc0d9db26b59bceb66a
Pradeep yadav <pradeep20020102@gmail.com>
2026-01-09 21:06:05 +0545

    hero section created

--- landingpage/src/components/hero.tsx
+++ landingpage/src/components/hero.tsx
@@ -1,6 +1,13 @@
 
 export const Hero = () => {
     return (
-        <div className="hero-root"></div>
+        <div className="flex flex-col items-center justify-center text-center my-20">
+            <div className="px-4 py-1.5 backdrop-blur-[10px] tracking-wide rounded-full border-transparent text-xs font-medium bg-neutral-200/50 text-black hover:bg-gray-200 cursor-pointer">We're hiring Founding Ruby Engineers
+            </div>
+            <h1 className="font-medium text-6xl mt-10 word-spacing-[0.02em] tracking-tight">
+                Magically simplify <br />accounting and taxes
+            </h1>
+            <p className="mt-5 text-[#595959] text-xl text-center tracking-tight">Automated bookkeeping. Effortless tax filing. Financial clarity. <br />Set up in 10 mins. Back to building by 6:59pm.</p>
+        </div>
     )
 } 
