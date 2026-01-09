4f69454e6d1c86d8cdd9f6e595cad8bed1a2a21f
Pradeep yadav <pradeep20020102@gmail.com>
2026-01-09 21:04:13 +0545

    hero section also included along with navbar

--- landingpage/src/app/page.tsx
+++ landingpage/src/app/page.tsx
@@ -1,12 +1,17 @@
 import { Container } from "@/components/container";
+import { Hero } from "@/components/hero";
 import { Navbar } from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div className="layout">
-        
+    <div className="flex flex-col items-center h-screen relative [background:radial-gradient(125%_100%_at_50%_0%,_#FFF_6.32%,_#E0F0FF_29.28%,_#E7EFFD_68.68%,_#FFF_100%)]">
+      {/* <div className="max-w-5xl mx-auto absolute inset-0 h-full w-full">
+        <div className="absolute inset-y-0 left-0 h-full w-px bg-linear-to-b from-neutral-300/50 via-neutral-200 to-transparent"></div>
+        <div className="absolute inset-y-0 right-0 h-full w-px bg-linear-to-b from-neutral-300/50 via-neutral-200 to-transparent"></div>
+      </div> */}
       <Container>
         <Navbar></Navbar>
+        <Hero/>
       </Container>
     </div>
   );
